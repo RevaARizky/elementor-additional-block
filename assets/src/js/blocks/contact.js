a56d1cb5083ed4e18ca1098cf3355449ee35a245
RevaARizky <revaathallah86@gmail.com>
2024-02-14 10:19:29 +0800

    form number arrow

--- assets/src/js/blocks/contact.js
+++ assets/src/js/blocks/contact.js
@@ -58,6 +58,45 @@
             })
         }, 2500)
 
+
+        arrowHandler = (act, el) => {
+
+        }
+        document.querySelectorAll('.custom-number-arrow').forEach(el => {
+            let par = el.querySelector('.forminator-field')
+            let input = el.querySelector('input')
+
+            let arrowUp = document.createElement('div')
+            arrowUp.classList.add('arrow-up')
+            arrowUp.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 14" fill="none"><path d="M15.4453 9.42107L10.6505 3.8647C10.6066 3.81392 10.5526 3.77325 10.4921 3.74539C10.4316 3.71753 10.366 3.70312 10.2996 3.70312C10.2333 3.70312 10.1676 3.71753 10.1071 3.74539C10.0466 3.77325 9.99266 3.81392 9.94875 3.8647L5.15301 9.42107" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
+            arrowUp.addEventListener('click', () => {
+                if(parseInt(input.value) >= parseInt(input.max)) {
+                    return false
+                }
+                input.value++
+            })
+            let arrowDown = document.createElement('div')
+            arrowDown.classList.add('arrow-down')
+            arrowDown.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="rotate: 180deg;" width="21" height="21" viewBox="0 0 21 14" fill="none"><path d="M15.4453 9.42107L10.6505 3.8647C10.6066 3.81392 10.5526 3.77325 10.4921 3.74539C10.4316 3.71753 10.366 3.70312 10.2996 3.70312C10.2333 3.70312 10.1676 3.71753 10.1071 3.74539C10.0466 3.77325 9.99266 3.81392 9.94875 3.8647L5.15301 9.42107" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
+            arrowDown.addEventListener('click', () => {
+                if(parseInt(input.value) <= 0) {
+                    return false
+                }
+                input.value--
+            })
+            let wrapper = document.createElement('div')
+            wrapper.classList.add('custom-number-wrapper')
+            wrapper.classList.add('relative')
+            wrapper.append(input)
+            wrapper.append(arrowUp)
+            wrapper.append(arrowDown)
+
+            par.append(wrapper)
+
+
+            
+        })
+
     })
 
 })(jQuery)
