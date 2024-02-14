629fb78464b1e90769ea023bd578f17cbddd2b49
RevaARizky <revaathallah86@gmail.com>
2024-02-14 15:33:33 +0800

    contact bug

--- assets/src/js/blocks/contact.js
+++ assets/src/js/blocks/contact.js
@@ -79,7 +79,7 @@
             arrowDown.classList.add('arrow-down')
             arrowDown.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="rotate: 180deg;" width="21" height="21" viewBox="0 0 21 14" fill="none"><path d="M15.4453 9.42107L10.6505 3.8647C10.6066 3.81392 10.5526 3.77325 10.4921 3.74539C10.4316 3.71753 10.366 3.70312 10.2996 3.70312C10.2333 3.70312 10.1676 3.71753 10.1071 3.74539C10.0466 3.77325 9.99266 3.81392 9.94875 3.8647L5.15301 9.42107" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
             arrowDown.addEventListener('click', () => {
-                if(parseInt(input.value) <= 0) {
+                if(parseInt(input.value) <= 0 || input.value == '') {
                     return false
                 }
                 input.value--
